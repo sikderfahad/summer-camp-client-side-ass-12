d064405dd248ebcb80bfcd23be6ce5d025f76a8f
unknown <sikderfahad>
2023-07-05 22:37:25 +0600

    authenticate initialize

--- src/provider/AuthProvider.jsx
+++ src/provider/AuthProvider.jsx
@@ -1,13 +1,75 @@
-import { createContext, useState } from "react";
+import { createContext, useEffect, useState } from "react";
+import app from "../firebase/firebase.config";
+import {
+  GithubAuthProvider,
+  GoogleAuthProvider,
+  createUserWithEmailAndPassword,
+  getAuth,
+  onAuthStateChanged,
+  signInWithEmailAndPassword,
+  signInWithPopup,
+  signOut,
+  updateProfile,
+} from "firebase/auth";
 
 export const AuthContext = createContext(null);
+
 const AuthProvider = ({ children }) => {
+  const auth = getAuth(app);
+  const googleProvider = new GoogleAuthProvider();
+  const gitHubProvider = new GithubAuthProvider();
+
   const [user, setUser] = useState("");
   const [loading, setLoading] = useState(true);
+
+  const googleUser = () => {
+    setLoading(true);
+    return signInWithPopup(auth, googleProvider);
+  };
+
+  const gitHubUser = () => {
+    setLoading(true);
+    return signInWithPopup(auth, gitHubProvider);
+  };
+
+  const profileName = (user, name) => {
+    setLoading(true);
+    return updateProfile(user, name);
+  };
+
+  const createUser = (email, pass) => {
+    setLoading(true);
+    return createUserWithEmailAndPassword(auth, email, pass);
+  };
+
+  const logOut = () => {
+    setLoading(true);
+    return signOut(auth);
+  };
+
+  const login = (email, pass) => {
+    setLoading(true);
+    return signInWithEmailAndPassword(auth, email, pass);
+  };
+
+  useEffect(() => {
+    const unsubscribe = onAuthStateChanged(auth, (createUser) => {
+      setUser(createUser);
+      setLoading(false);
+    });
+
+    return () => unsubscribe();
+  }, [auth]);
 
   const authInfo = {
     user,
     loading,
+    googleUser,
+    gitHubUser,
+    profileName,
+    createUser,
+    logOut,
+    login,
   };
 
   return (
